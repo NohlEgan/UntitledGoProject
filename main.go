a11a2080ad26d72f28dad086069bccc136b12b9f
Nohl Egan <39224642+NohlEgan@users.noreply.github.com>
2022-04-06 22:40:05 -0400

    Testing out some things on the web server
    I've set up another handler function which prints out the message "Hello World" 10 times on the pattern "/tenhelloworlds"

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,10 +21,27 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello!")
 }
 
+func tenHelloWorldsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/tenhelloworlds" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+
+	for i := 0; i < 10; i++ {
+		fmt.Fprintf(w, strconv.Itoa(i+1)+": Hello World!\n")
+	}
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/tenhelloworlds", tenHelloWorldsHandler)
 
 	fmt.Printf("Starting server on port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
