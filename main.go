ef2a9be7e6d03862445379a72557c9879590b433
Nohl Egan <39224642+NohlEgan@users.noreply.github.com>
2022-04-07 20:56:15 -0400

    More testing
    Now I've tried something where I have two different patterns that use the same handler function.
    
    The handler function uses an if statement to print out different results depending on the path the user took to get there.

--- main.go
+++ main.go
@@ -37,11 +37,28 @@ func tenHelloWorldsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func rightWayHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+
+	if r.URL.Path == "/therightway" {
+		fmt.Fprintf(w, "You got to this page the right way!")
+	} else if r.URL.Path == "/thewrongway" {
+		fmt.Fprintf(w, "You got to this page the wrong way!")
+	} else {
+		http.Error(w, "404 not found", http.StatusNotFound)
+	}
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/tenhelloworlds", tenHelloWorldsHandler)
+	http.HandleFunc("/therightway", rightWayHandler)
+	http.HandleFunc("/thewrongway", rightWayHandler)
 
 	fmt.Printf("Starting server on port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
