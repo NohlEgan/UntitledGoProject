eeb3047e8a34021137a45d90564c42fb5f7aa878
Nohl Egan <39224642+NohlEgan@users.noreply.github.com>
2022-05-03 17:28:11 -0400

    Fixed Issue(?)
    I commented out a few lines that I thought may be unnecessary. I'm no longer getting the 'http: superfluous response.WriteHeader call' message.
    
    Though I will have to do some more inspection to make sure no other issues have been created due to this change.

--- main.go
+++ main.go
@@ -81,14 +81,14 @@ func fruitHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
-	w.WriteHeader(status)
+	//w.WriteHeader(status)
 	if status == http.StatusNotFound {
 		http.ServeFile(w, r, "./static/404.html")
 	}
 }
 
 func main() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	//http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/tenhelloworlds", tenHelloWorldsHandler)
